acac7a542e70dd70b6688b05fd4ebc783907f506
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 06:37:38 +0000

    commite variable file

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 1332
+
+let accountEmail = "piyushsingh123@gmail.com"
+
+var accountPassword = "12332"
+
+accountCity = "Jaipur"
+
+let accountState;
+//accountId = 13 // not allowed
+
+console.log(accountId);
+
+accountCity = "Jammu"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]) 
+
+
+/*
+prefer not to use 
+becoz of issue in block and functional scope
+*/
