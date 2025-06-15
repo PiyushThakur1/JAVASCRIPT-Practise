47e545ef13322bbb121e64d4236780ce84a19265
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 07:05:04 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+let Score = "d123"
+
+console.log(typeof Score);
+console.log(typeof (Score));
+
+let valuInNumber = Number(Score)
+
+console.log(typeof valuInNumber);
+
+console.log(valuInNumber);
+
+
+// "435" = 435
+// "435adw" = NaN
+// true => 1; false => 0
+// "" => false
+// "piyuh" => true
