3613e48c3f0e2919394f5f6a9daaaee29787ba09
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 06:54:37 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS Code as newer version
+
+//alert(hello) // we are using node js not browser
+
+console.log(3
+    +
+    3); //code readabilty should be high
+
+
+let name = "piyush"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to the power of 53(range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined => value that is not defined
+// symbol =>unique
+
+
+// object
+
+console.log(typeof "piyush")
