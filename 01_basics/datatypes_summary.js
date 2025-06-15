403d108fd5a8e78eb01f214f6d2ba5ca004e737a
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 08:53:58 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number , boolean, null, undefined, Symbol, 
+// BigInt
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemo = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber =   876545678765678765678n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heroes =["shaktiman", "nagraj", "doga"]
+let obj = {
+    name: "Piyush",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello");
+    
+}
+
+console.log(typeof id);
+
+
