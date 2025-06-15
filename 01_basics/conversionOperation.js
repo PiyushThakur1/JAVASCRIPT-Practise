ad6b09563e2db2403d7faff27b1ce50c6a4cb513
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 07:31:44 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,13 +1,13 @@
 let Score = "d123"
 
-console.log(typeof Score);
-console.log(typeof (Score));
+//console.log(typeof Score);
+//console.log(typeof (Score));
 
 let valuInNumber = Number(Score)
 
-console.log(typeof valuInNumber);
+//console.log(typeof valuInNumber);
 
-console.log(valuInNumber);
+//console.log(valuInNumber);
 
 
 // "435" = 435
@@ -15,3 +15,38 @@ console.log(valuInNumber);
 // true => 1; false => 0
 // "" => false
 // "piyuh" => true
+
+
+
+// ********************************* Operations ***********************
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello1"
+let str2 = " Piyush"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((2+4) * 5);
+
+
+console.log(+true);
+console.log("");
+
