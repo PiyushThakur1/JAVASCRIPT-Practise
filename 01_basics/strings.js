a3abe7950a83eb7a02370403fd39cd5dd41473cd
Piyush Singh <109173571+PiyushThakur1@users.noreply.github.com>
2025-06-15 17:54:29 +0000

    string

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,5 +1,37 @@
-const name = "Piyush"
+const name = "Piyush-s"
 
 const repo = 10
 
 console.log(`name is ${name} and repo count is ${repo}`);
+
+
+const gameName = new String('Piyush-s')
+
+// console.log(gameName[0])
+// // console.log(gameName.__proto__)
+
+// // console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+
+console.log(gameName.indexOf('h'))
+
+const newString = gameName.substring(0,5)
+
+console.log(newString);
+
+
+const anstring = gameName.slice(-5,3)
+
+console.log(anstring);
+
+
+const newstr1 = "   piyush    "
+
+console.log(newstr1.trim());
+
+const url = "https:/google.com/gooogle%20com"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('google'));
+
+console.log(gameName.split('-'));
